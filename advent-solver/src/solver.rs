82180828ba1b9691c9b56edd2232aec7917b8b75
Quentin Doussot <quentin.doussot@jellysmack.com>
2021-12-04 13:01:30 +0100

    Add Day 3 part one

--- advent-solver/src/solver.rs
+++ advent-solver/src/solver.rs
@@ -1,6 +1,15 @@
 #[derive(Debug)]
 pub enum Error {
-    WrongInput,
+    UnimplementedSolver,
+    WrongLine(usize, FormatError),
+    ExpectationUnfulfilled,
+    Unexpected,
+}
+
+#[derive(Debug)]
+pub enum FormatError {
+    UnexpectedCharacter,
+    WrongLenght(usize),
 }
 
 pub trait Solver {
@@ -9,4 +18,12 @@ pub trait Solver {
 
 pub fn new<So: Default>() -> Box<So> {
     Box::new(So::default())
+}
+
+#[derive(Default)]
+pub struct Unimplemented {}
+impl Solver for Unimplemented {
+    fn solve(&self, _lines: &[String]) -> Result<String, Error> {
+        Err(Error::UnimplementedSolver)
+    }
 }
